cc0e18014325deeac024bb78c5dcecb9df271c17
Victor MACHRY VERZA <victor.machry-verza@ecl20.ec-lyon.fr>
2023-01-25 15:16:36 +0100

    Changed App Component to a Class Component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import React from 'react';
 import PokedexHeader from './Components/PokedexHeader';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <PokedexHeader author={"@victor.verza"} name={"Greg"} />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      name: "Gregory"
+    }
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <PokedexHeader author={"@victor.verza"} name={this.state.name} />
+      </div>
+    );
+  }
 }
 
 export default App;
