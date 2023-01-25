0cff882fc306f2c8a73089dee0d0580ace7dfdc3
Victor MACHRY VERZA <victor.machry-verza@ecl20.ec-lyon.fr>
2023-01-25 15:52:06 +0100

    Added an Imput Form that is tied to Local State

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import PokedexHeader from './Components/PokedexHeader';
+import { Input, Segment } from 'semantic-ui-react';
 import './App.css';
 
 class App extends React.Component {
   constructor(props){
     super(props)
-    this.state = {
-      name: "Gregory"
-    }
+    this.state = {name: "Greg", author: "@victor.verza"}
+    this.onChange = this.onChange.bind(this)
   }
   
+  onChange(event, data) {
+    console.log(data.value)
+    this.setState({name: data.value})
+  }
+
   render() {
     return (
       <div className="App">
-        <PokedexHeader author={"@victor.verza"} name={this.state.name} />
+        <PokedexHeader author={this.state.author} name={this.state.name} />
+        <Segment basic>
+          <Input 
+          label={"Input Form"}
+          onChange={this.onChange}
+          value={this.state.name}
+          />
+        </Segment>
       </div>
     );
   }
